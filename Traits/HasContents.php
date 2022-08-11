1efbecac1222bd4e714c1834eeb0b540a49bd16b
Codrin Axinte <codrin.axinte@outlook.com>
2022-08-11 15:05:09 +0300

    Added indexing helper method

--- Traits/HasContents.php
+++ Traits/HasContents.php
@@ -2,6 +2,7 @@
 
 namespace Modules\PageBuilder\Traits;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
@@ -10,6 +11,8 @@ use Modules\PageBuilder\Models\Content;
 
 /**
  * @mixin Model
+ * @property-read Content|null contentPublished
+ * @property-read Collection contentsPublished
  */
 trait HasContents
 {
@@ -29,6 +32,25 @@ trait HasContents
         static::deleted(function ($model) {
             $model->contents()->delete();
         });
+    }
+
+    protected function getContentsIndexing(): array
+    {
+        return $this
+            ->contentsPublished()
+            ->get()
+            ->mapWithKeys(function (Content $content) {
+                try {
+                    return ['content_' . $content->locale => $content->getIndexData()];
+                } catch (\Exception $exception) {
+                    // Skip, or report this
+                }
+
+                return null;
+
+            })
+            ->filter()
+            ->toArray();
     }
 
     /**
